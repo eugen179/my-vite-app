0fd7251e468b5b2467ba8af31b290b6856afad7b
eugen179 <maisheugene@gmail.com>
2024-10-18 09:16:22 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+      
+      </div>
+      <h1>Eugene</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
